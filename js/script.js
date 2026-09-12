c1c32251df826ad27c934b6b127ed45841515c51
abdillah <frixeevender@gmail.comn>
2026-09-12 22:29:19 +0700

    fix: rapikan landing page UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const promoButton = document.querySelector("#promoButton");
 
 promoButton.addEventListener("click", () => {
-     promoButton.textContent = "Promo: Beli 2 gratis tester!";
+     promoButton.textContent = "Promo: beli 2 gratis tester!";
      console.log("Promo Rumah Samudra Kopi berhasil ditampilkan.");
 });
