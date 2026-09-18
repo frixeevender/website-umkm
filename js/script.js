141541a5085f2e6acc72ad6cf4f16eb29f11c228
abdillah <frixeevender@gmail.comn>
2026-09-18 19:57:38 +0700

    update website UMKM

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const promoButton = document.querySelector("#promoButton");
 
-promoButton.addEventListener("click", () => {
-     promoButton.textContent = "Promo: beli 2 gratis tester!";
-     console.log("Promo Rumah Samudra Kopi berhasil ditampilkan.");
-});
+if (promoButton) {
+     promoButton.addEventListener("click", () => {
+          promoButton.textContent = "Promo: beli 2 gratis tester!";
+          console.log("Promo Rumah Samudra Kopi berhasil ditampilkan.");
+     });
+}
